4712f83f111364bb163cbf395aa849e76c897938
Darya2021 <84893917+Darya2021@users.noreply.github.com>
2021-05-27 15:06:02 +0430

    Create code1.py

---
+++ code1.py
@@ -0,0 +1,11 @@
+print ("inter a number:")
+number_1= input()
+number_1_float= float(number_1)
+
+print ("inter a number:")
+number_2= input()
+number_2_float= float(number_2)
+
+result= number_1_float + number_2_float
+
+print(result)
