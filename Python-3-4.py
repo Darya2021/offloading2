49d790f9d9e616ef226ec874827202f8689b0c9a
Darya2021 <84893917+Darya2021@users.noreply.github.com>
2021-06-03 15:51:08 +0430

    Add files via upload

---
+++ Python-3-4.py
@@ -0,0 +1,7 @@
+def mohit_mostatil(tol, arz):
+    mohit= (tol + arz) * 2
+    print("mohite mostatil = ", mohit)
+
+tol= int(input("tol ra vared konid\n"))
+arz= int(input("arz ra vared konid\n"))
+mohit_mostatil(tol, arz)
