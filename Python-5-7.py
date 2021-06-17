65155eb25388745a54d815c115802e44e50b7b7a
Darya2021 <84893917+Darya2021@users.noreply.github.com>
2021-06-17 16:47:57 +0430

    Add files via upload

---
+++ Python-5-7.py
@@ -0,0 +1,5 @@
+gender= ["agha", "agha", "khanom", "agha", "agha", "khanom", "khanom", "khanom"]
+classlist= ["amin", "meysam", "mahnaz", "reza", "farokh", "mobina", "darya", "paniz"]
+
+for gen, list in zip(gender, classlist):
+    print(f'salam {gen} {list}")
