af7a0596bd4ddcbb7c6928816d26702c44e26e66
Darya2021 <84893917+Darya2021@users.noreply.github.com>
2021-06-17 13:41:43 +0430

    Add files via upload

---
+++ Python-4-Homework6-hazfanasoretekrarilist.py
@@ -0,0 +1,8 @@
+### hazf anasor tekrari list ###
+classlist= ["amin", "reza", "meysam", "mahnaz", "reza", "paniz", "farokh", "mobina",\
+ "reza", "darya", "paniz", "reza", "paniz"]
+print(classlist)
+print(len(classlist))
+newclasslist= list(set(classlist))
+print(newclasslist)
+print(len(newclasslist))
