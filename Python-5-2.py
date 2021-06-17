28cacc1166631bf93753dac9de400231d4389399
Darya2021 <84893917+Darya2021@users.noreply.github.com>
2021-06-17 15:06:02 +0430

    Add files via upload

---
+++ Python-5-2.py
@@ -0,0 +1,7 @@
+#classlist= ["amin", "meysam", "mahnaz", "reza", "farokh", "mobina", "darya", "paniz"]
+aboutpython= "python y zabane barname nevisie k emroze alaghemandane ziadi dare\
+ chon estefade az on baraye karbaran b nesbate zabanhaye ghabli sade tar hast".split("b")
+
+ for name in aboutpython:
+     if len(name) !=1:
+         print(name)
