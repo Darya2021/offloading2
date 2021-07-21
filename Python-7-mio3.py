a44e120d20b7ee25bcd3edd20af7ef9073c92170
Darya2021 <84893917+Darya2021@users.noreply.github.com>
2021-07-22 02:16:51 +0430

    Add files via upload

---
+++ Python-7-mio3.py
@@ -0,0 +1,12 @@
+class Cat:
+    def __init__ (self, name, age, color):
+        self.name = name
+        self.age = age
+        self.color = color
+
+
+pishi = Cat("pishi", 3, "red")
+
+print(pishi.name)
+print(pishi.age)
+print(pishi.color)
