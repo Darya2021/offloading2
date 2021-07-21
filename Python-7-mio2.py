20048928eedf1470480316d563e50ed606d0c80e
Darya2021 <84893917+Darya2021@users.noreply.github.com>
2021-07-22 01:15:18 +0430

    Add files via upload

---
+++ Python-7-mio2.py
@@ -0,0 +1,18 @@
+class Cat:
+    name = ""
+    age = 0
+    color = ""
+
+    def hello():
+        print("mio mio!!")
+
+pishi = Cat()
+pishi.name = "pishi"
+pishi.age = 2
+pishi.color = "yellow"
+
+print(pishi.name)
+print(pishi.age)
+print(pishi.color)
+
+Cat.hello()
